05357d7983d0fc82397db5af0d45c2f94f17ec0c
Joost van der Schee <jhvanderschee@gmail.com>
2017-01-10 17:28:36 +0100

    update

---
+++ _includes/js/custom.js
@@ -0,0 +1,129 @@
+function iOSversion(useragent) {
+  if (/iP(hone|od|ad|od Touch)/.test(useragent)) {
+    // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+    var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+  }
+  return false;
+}
+$(window).on('orientationchange', function(e) {
+	window.location.reload();
+});
+
+$( document ).ready(function() {
+
+
+	var useragent = navigator.userAgent;
+	//check for old stock Android browser
+	//to remove position fixed and background attachment fixed
+	if(	(useragent.indexOf("Mozilla/5.0") > -1) &&
+		(useragent.indexOf("Android") > -1) &&
+		(useragent.indexOf("Chrome") == -1)
+	) {
+		$('body').addClass('old_Android');
+	}
+
+	//check for Android
+	//to remove animation
+	if(useragent.indexOf("Android") > -1) $('body').addClass('Android');
+
+	//check for old iOS version 
+	//to remove position fixed
+	var iOS_version = iOSversion(useragent);
+	if(iOS_version[0]<5) $('body').addClass('old_iOS');
+	if(iOS_version[0]>6) $('body').addClass('new_iOS');
+
+	//check for iOS 
+	//to remove background attachment fixed and animation
+	var iOS = /(iPad|iPhone|iPod)/g.test(useragent);
+	if(iOS) $('body').addClass('iOS');
+
+	//detect browser with Touch Events running on touch-capable device
+	if ('ontouchstart' in window) {
+	     $('body').addClass('touch');
+	}
+
+	if(!$('body').hasClass('old_Android') && !$('body').hasClass('old_iOS')) {
+		//if modern browser
+	}
+
+	if($('body').hasClass('Android') || $('body').hasClass('iOS')) {
+		//no animation
+	}
+	else {
+		//animation
+	}
+
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.item'
+	});
+	// bind filter button click
+	$('.filters-button-group').on( 'click', 'button', function() {
+	  var filterValue = $( this ).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
+	  $( window ).resize();
+	});
+	
+	if(window.location.hash) {
+    // Fragment exists
+		$('.button').each(function() {
+			var hashFilter = '.'+window.location.hash.substring(1);
+			if($(this).attr('data-filter') == hashFilter) {
+				$('.filters-button-group').find('.is-checked').removeClass('is-checked');
+      	$('.filters-button-group').find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
+				$grid.isotope({ filter: hashFilter });
+			}
+		});
+	}
+
+
+  $( "img" ).load(function() {
+  		$( window ).resize();
+	});
+
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname && !$(this).hasClass('carousel-control')) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+	$('.navbar-toggle.top').click( function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		if($('.navbar-nav > li:first-child').css('opacity')=='0') {
+			//show nav
+			$('.navbar-nav > li').stop( true, true ).delay(300).animate({
+				opacity: 1
+  		}, 200, function() {
+    		// Animation complete.
+  		});
+		} else {
+			//hide nav
+			$('.navbar-nav > li:visible').not(':nth-child(4)').stop( true, true ).delay(300).animate({
+				opacity: 0
+  		}, 200, function() {
+    		// Animation complete.
+  		});
+		}
+	});
+	$( window ).resize();
+});
+
+
+$( window ).resize(function() {
+	$('body').css('height','auto');
+	$('footer').css('position','static');
+	if($('body').height() <= $('html').height()) {
+		$('body').css('height','100%');
+		$('footer').css('position','absolute');
+	}
+});
---
+++ index.md
@@ -0,0 +1,14 @@
+---
+layout: portfolio
+title: We hope to find our madness within your company
+headers:
+  - image: /uploads/header1.png
+    title: "In the wind of Sweden"
+    description: "Editorial for National Geographic"
+    url: /item
+  - image: /uploads/header1.png
+    title: "In the wind of Sweden"
+    description: "Editorial for National Geographic"
+    url: /item
+---
+
