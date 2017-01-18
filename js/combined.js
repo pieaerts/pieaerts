01c21f441860c7cf4289dfea806bceb89b3b79a4
Joost van der Schee <jhvanderschee@gmail.com>
2017-01-18 10:44:29 +0100

    update

--- js/combined.js
+++ js/combined.js
@@ -4,4 +4,5 @@ layout:
 {% include js/jquery-1.11.2.min.js %}
 {% include js/bootstrap_full.min.js %}
 {% include js/jasny-bootstrap.min.js %}
+{% include js/fastclick.js %}
 {% include js/custom.js %}
